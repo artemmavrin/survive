853842d3eff140dcbe891e016660fcdfd49ad338
Artem Mavrin <amavrin@ucsd.edu>
2018-07-20 17:05:19 -0700

    Added a Kaplan-Meier unit test

--- survive/univariate/test/test_kaplan_meier.py
+++ survive/univariate/test/test_kaplan_meier.py
@@ -1,8 +1,10 @@
 """Unit tests for the Kaplan-Meier estimator implementation."""
 
+import itertools
 import unittest
 
 import numpy as np
+import pandas as pd
 
 from survive.univariate import KaplanMeier
 
@@ -16,24 +18,27 @@ class TestKaplanMeier(unittest.TestCase):
         empirical survival function, shown below.
 
                 |
-            1.0 |----+
-                |    |
-            0.8 |    +----+
-                |         |
-            0.6 |         +----+
-                |              |
-            0.4 |              +----+
-                |                   |
-            0.2 |                   +----+
-                |                        |
-            0.0 +----+----+----+----+----+----
-                0    1    2    3    4    5
+            1.0 |-----+
+                |     |
+            0.8 |     +-----+
+                |           |
+            0.6 |           +-----+
+                |                 |
+            0.4 |                 +-----+
+                |                       |
+            0.2 |                       +-----+
+                |                             |
+            0.0 +-----+-----+-----+-----+-----+-----
+                0     1     2     3     4     5
 
         This test just verifies this and does some other sanity checks.
         """
         time = [1, 2, 3, 4, 5]
         km = KaplanMeier()
         km.fit(time)
+
+        # There should only be one group
+        self.assertEqual(km.data.n_groups, 1)
 
         # Check estimated survival probabilities
         data = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
@@ -47,14 +52,103 @@ class TestKaplanMeier(unittest.TestCase):
             self.assertIsInstance(val, np.ndarray)
             self.assertEqual(val.shape, (len(data),))
 
-        # There should only be one group
-        self.assertEqual(km.data.n_groups, 1)
-
         # Calling predict(), var(), se() with a scalar argument should return
         # a float. Calling ci() should return a pair of floats.
         for t in data:
             lower, upper = km.ci(t)
             for val in (km.predict(t), km.var(t), km.se(t), lower, upper):
+                self.assertIsInstance(val, float)
+
+    def test_no_censoring_two_groups(self):
+        """Simple example with two group and no censoring.
+
+        Group a: 1, 2, 3, 4, 5
+                |
+            1.0 |-----+
+                |     |
+            0.8 |     +-----+
+                |           |
+            0.6 |           +-----+
+                |                 |
+            0.4 |                 +-----+
+                |                       |
+            0.2 |                       +-----+
+                |                             |
+            0.0 +-----+-----+-----+-----+-----+-----
+                0     1     2     3     4     5
+
+        Group b: 1.5, 2.5, 3.5, 4.5, 5.5
+                |
+            1.0 |--------+
+                |        |
+            0.8 |        +-----+
+                |              |
+            0.6 |              +-----+
+                |                    |
+            0.4 |                    +-----+
+                |                          |
+            0.2 |                          +-----+
+                |                                |
+            0.0 +--+-----+-----+-----+-----+-----+--
+                0 0.5   1.5   2.5   3.5   4.5   5.5
+        """
+        time = [1, 2, 3, 4, 5, 1.5, 2.5, 3.5, 4.5, 5.5]
+        group = ["a", "a", "a", "a", "a", "b", "b", "b", "b", "b"]
+        km = KaplanMeier()
+        km.fit(time, group=group)
+
+        # There should be two groups
+        self.assertEqual(km.data.n_groups, 2)
+
+        # Check estimated survival probabilities for each group
+        data = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6]
+        survival0 = [1., 1., 0.8, 0.8, 0.6, 0.6, 0.4, 0.4, 0.2, 0.2, 0., 0., 0.]
+        survival1 = [1., 1., 1, 0.8, 0.8, 0.6, 0.6, 0.4, 0.4, 0.2, 0.2, 0., 0.]
+        np.testing.assert_almost_equal(km.predict(data, "a"), survival0)
+        np.testing.assert_almost_equal(km.predict(data, "b"), survival1)
+
+        # Check estimated survival probabilities for both groups at once
+        prob = km.predict(data)
+        self.assertIsInstance(prob, pd.DataFrame)
+        np.testing.assert_equal(prob.columns.values, ["a", "b"])
+        np.testing.assert_almost_equal(prob["a"], survival0)
+        np.testing.assert_almost_equal(prob["b"], survival1)
+
+        # Calling predict(), var(), se() with an array argument should return
+        # a DataFrame. Calling ci() should return a pair of DataFrames.
+        lower, upper = km.ci(data)
+        for val in (km.predict(data), km.var(data), km.se(data), lower, upper):
+            self.assertIsInstance(val, pd.DataFrame)
+            np.testing.assert_equal(val.columns.values, ["a", "b"])
+            self.assertEqual(val.shape, (len(data), 2))
+
+        # Calling predict(), var(), se() with a scalar argument should return
+        # a DataFrame. Calling ci() should return a pair of DataFrames.
+        for t in data:
+            lower, upper = km.ci(t)
+            for val in (km.predict(t), km.var(t), km.se(t), lower, upper):
+                self.assertIsInstance(val, pd.DataFrame)
+                np.testing.assert_equal(val.columns.values, ["a", "b"])
+                self.assertEqual(val.shape, (1, 2))
+
+        # Calling predict(), var(), se() with an array argument and specifying a
+        # group should return an array Calling ci() should return a pair of
+        # arrays.
+        for group in ("a", "b"):
+            lower, upper = km.ci(data, group=group)
+            for val in (km.predict(data, group=group),
+                        km.var(data, group=group), km.se(data, group=group),
+                        lower, upper):
+                self.assertIsInstance(val, np.ndarray)
+                self.assertEqual(val.shape, (len(data),))
+
+        # Calling predict(), var(), se() with a scalar argument and a specified
+        # group should return a float. Calling ci() should return a pair of
+        # floats.
+        for group, t in itertools.product(("a", "b"), data):
+            lower, upper = km.ci(t, group=group)
+            for val in (km.predict(t, group=group), km.var(t, group=group),
+                        km.se(t, group=group), lower, upper):
                 self.assertIsInstance(val, float)
 
 
